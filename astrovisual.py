7f2e6d3f18465283544a9a88226a810c40d5c146
Felipe <felipe8ffs@gmail.com>
2023-05-26 12:06:40 -0300

    Visual melhorado

--- astrovisual.py
+++ astrovisual.py
@@ -1,9 +1,14 @@
 import requests
 import os
 
+# Colors
+default = "\033[0;0m"
+gold = "\033[0;33m"
+red = "\033[0;31m"
+
 
 def astro_visual():
-    print(f"{'Astro':=^53}")
+    print(f"{'Astro':=^55}")
     username = str(input("Nome de Usuário: "))
     password = str(input("Senha: "))
 
@@ -11,22 +16,23 @@ def astro_visual():
 
     try:
         req = requests.get(f"http://localhost:5136/api/ZodiacProfiles/Login/{username}/{password}")
-    except requests.exceptions.RequestException as e:
-        print("Servidor fora do ar")
+    except requests.exceptions.RequestException:
+        print(f"{red}Erro: Servidor fora do ar{default}")
         return False
     else:
         if req.status_code != 200:
-            print(f"Usuário ou Senha incorreto")
+            print(f"{red}Erro: Usuário ou Senha incorreto{default}")
             return False
         userdata = req.json()
 
     os.system('cls')
-    print(f"{'Astro':=^55}")
+    print(f"{'Astro Profile':=^55}")
     print(f'Nome: {userdata["name"]}')
     print(f'Sobre Mim: {userdata["description"]}')
     print(f'Signo: {userdata["signName"]}')
-    print(f'Membro: { "Premium" if  userdata["membership"] else "Free"}')
+    print(f'Plano: { f"{gold}Premium{default}" if  userdata["membership"] else "Gratuito"}')
 
+    print(f"{'Informações do Signo':=^55}")
     sign_data = userdata["signData"]
 
     print(f'Elemento: {sign_data["element"]}')
@@ -35,8 +41,8 @@ def astro_visual():
     print(f'Flor: {sign_data["flower"]}')
 
     if userdata["membership"]:
-        print(f'Bebida do Dia: {sign_data["drink"]}')
-        print(f'Sorte no Amor: {sign_data["love"]}')
+        print(f'{gold}Bebida do Dia: {sign_data["drink"]}')
+        print(f'Sorte no Amor: {sign_data["love"]}{default}')
 
     return True
 
@@ -45,16 +51,15 @@ class AstroVisual:
     if __name__ == '__main__':
         while True:
             if not astro_visual():
-                logoff_input = input("Pressione 1 para tentar novamente ou 2 para sair\n")
+                logoff_input = input(f"{'[1] Tentar Novamente'}{'[Qualquer Tecla] Sair':>35}\n")
                 os.system('cls')
                 if logoff_input == '1':
                     continue
                 else:
                     break
-                continue
             else:
                 print(f'=' * 55)
-                logoff_input = input("Pressione 1 para fazer logout ou 2 para sair\n")
+                logoff_input = input(f"{'[1] Deslogar'}{'[Qualquer Tecla] Sair':>43}\n")
                 os.system('cls')
                 if logoff_input == '1':
                     continue
